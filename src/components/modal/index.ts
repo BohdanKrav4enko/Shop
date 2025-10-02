18812f1546bb3020067050fe700677695a816cd9
BohdanKrav4enko <bohdan.krav4enko@gmail.com>
2025-10-02 10:26:23 +0300

    fix

--- src/components/modal/index.ts
+++ src/components/modal/index.ts
@@ -1,8 +1,9 @@
+export {ModalFooter,} from "@/components/modal/modals/cartModal/ModalFooter";
+export {ShoppingCart} from "@/components/modal/modals/cartModal/ShoppingCart"
 export {Modal} from "./Modal"
 export {AdminModal} from "./modals/adminModal/AdminModal"
 export {RemoveItemModal} from "./modals/RemoveItemModal"
 export {SearchModal} from "./modals/SearchModal"
-export {ShoppingCart} from "@/components/modal/modals/shoppingCart/ShoppingCart"
 export {UserRoleStatus} from "./modals/adminModal/adminModalComponents/UserRoleStatus"
 export {Favorites} from "./modals/favoritesModal/Favorites"
 export {
@@ -20,4 +21,3 @@ export {
 export {
     ModalFooterContainer, CartLayout, CartTotal, TotalContainer, CartContent, ClearButton
 } from "./styles/ShoppingCartStyle"
-export {ModalFooter} from "@/components/modal/modals/shoppingCart/ModalFooter.tsx";
