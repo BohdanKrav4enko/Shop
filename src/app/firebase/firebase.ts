8aa1243de98d30f5ca1fbd1304868f63da49968a
BohdanKrav4enko <bohdan.krav4enko@gmail.com>
2025-10-05 22:58:00 +0300

    fix

--- src/app/firebase/firebase.ts
+++ src/app/firebase/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB2pPhyW2tjZrSNrtpupgBIt1d5QXBJbYY",
-    authDomain: "shop-a261c.firebaseapp.com",
-    projectId: "shop-a261c",
-    storageBucket: "shop-a261c.firebasestorage.app",
-    messagingSenderId: "602329759071",
-    appId: "1:602329759071:web:f3e8dd97f1d5adce664aea",
-    measurementId: "G-8ZMCY0ZRCP"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ import {GlobalStyle} from "./styles/GlobalStyle.ts";
 import {HashRouter} from "react-router-dom";
 import {I18nextProvider} from "react-i18next";
 import i18n from "./i18n.ts";
-
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <Provider store={store}>
